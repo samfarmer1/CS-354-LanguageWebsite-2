7a57624a3df44c78c18c716c1dd8279f0c60aa8d
javitrejo14 <57146166+javitrejo14@users.noreply.github.com>
2022-04-25 23:33:58 -0600

    Removed Non-needed constructor

--- Kotlin/SavingAccount.kt
+++ Kotlin/SavingAccount.kt
@@ -2,12 +2,6 @@ public class SavingAccount (number: String, customer: Customer, balance: Double)
 
     private var interest: Double = 0.0;
 
-    fun SavingAccount(number: String, customer: Customer, balance: Double){
-        this.number = number;
-        this.customer = customer;
-        this.balance = balance;
-    }
-
     fun accrue(rate: Double){
         interest += balance * rate;
         balance += balance * rate;
